df505f0f2e0b98df69b64f7b0421f05c3372382a
Patrik Patera <pat.patera@gmail.com>
2017-10-04 23:43:39 +0200

    Check if was pressed Ctr+Z in the shell.

--- src/kernel/io.cpp
+++ src/kernel/io.cpp
@@ -1,12 +1,14 @@
 #include "io.h"
 #include "kernel.h"
 #include "handles.h"
+#include <memory>
+
+
 
 void HandleIO(kiv_os::TRegisters &regs) {
 
 	//V ostre verzi pochopitelne do switche dejte volani funkci a ne primo vykonny kod
 	
-
 	switch (regs.rax.l) {
 			case kiv_os::scCreate_File: {
 				HANDLE result = CreateFileA((char*)regs.rdx.r, GENERIC_READ | GENERIC_WRITE, (DWORD)regs.rcx.r, 0, OPEN_EXISTING, 0, 0);
@@ -47,6 +49,21 @@ void HandleIO(kiv_os::TRegisters &regs) {
 			if (!regs.flags.carry) regs.rax.r = read;
 			else regs.rax.r = GetLastError();
 
+			
+			char* b = reinterpret_cast<char*>(regs.rdi.r);
+
+			int stdinn = read > 0;
+
+			if (stdinn) {
+				stdinn = ! ((read>2) &&		
+					(b[read - 3] == 0x1a) & (b[read - 2] == 0x0d) & (b[read - 1] == 0x0a));
+
+				if ((!stdinn) & (read>2)) read -= 3;
+
+			}
+
+			b = NULL;
+			
 			break;
 		}
 	}
