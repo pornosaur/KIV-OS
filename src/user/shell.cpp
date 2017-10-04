9f5024a3b632f43f2c12c1beae7f2e2459ab5c91
Patrik Patera <pat.patera@gmail.com>
2017-10-04 09:20:03 +0200

    Added waiting for input in console.

--- src/user/shell.cpp
+++ src/user/shell.cpp
@@ -3,12 +3,18 @@
 #include "rtl.h"
 
 #include <cstring>
+#include <cstdlib>
 
 size_t __stdcall shell(const kiv_os::TRegisters &regs) {
 	kiv_os::THandle stdin = kiv_os_rtl::Create_File("CONOUT$", /*FILE_SHARE_WRITE*/2);	//nahradte systemovym resenim, zatim viz Console u CreateFile na MSDN
+	kiv_os::THandle stdio = kiv_os_rtl::Create_File("CONIN$", /*FILE_SHARE_READ*/1);
 	const char* hello = "Hello world!\n";
+	char *input = (char *)malloc(sizeof(char) * 255);
 	size_t written;
+	size_t read;
 	kiv_os_rtl::Write_File(stdin, hello, /*strlen(hello)*/13, written);
+	kiv_os_rtl::Read_File(stdio, input, 255, read);
+	kiv_os_rtl::Write_File(stdin, input, read, written);
 	kiv_os_rtl::Close_File(stdin);
 	return 0;
 }
