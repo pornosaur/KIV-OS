312662aab446c02ac2abeea917d26fdfb9cc3382
Patrik Patera <pat.patera@gmail.com>
2017-10-15 21:40:45 +0200

    #3
    + Implemented part of echo cmd.
    + Added waiting for input in console.
    + Added regex to match cmds.
    + Added regex to match redirects '>', '>>' in input params.

--- src/user/shell.cpp
+++ src/user/shell.cpp
@@ -1,34 +1,105 @@
 #include "shell.h"
 
-#include "rtl.h"
 
 #include <cstring>
 #include <cstdlib>
+#include <regex>
+#include <ctype.h>
+
 
 size_t __stdcall shell(const kiv_os::TRegisters &regs) {
-	kiv_os::THandle stdin = kiv_os_rtl::Create_File("CONOUT$", /*FILE_SHARE_WRITE*/2);	//nahradte systemovym resenim, zatim viz Console u CreateFile na MSDN
-	kiv_os::THandle stdio = kiv_os_rtl::Create_File("CONIN$", /*FILE_SHARE_READ*/1);
-	const char* hello = "Hello world!\n";
-	char *input = (char *)malloc(sizeof(char) * 255);
-	size_t written;
-	size_t read;
-	kiv_os_rtl::Write_File(stdin, hello, /*strlen(hello)*/13, written);
-	kiv_os_rtl::Read_File(stdio, input, 255, read);
-	kiv_os_rtl::Write_File(stdin, input, read, written);
-	kiv_os_rtl::Close_File(stdin);
+	/* First group => command; Second group => parametrs */
+	static const std::regex regex_cmd("\\s*(\\w+)\\s*(.*)");
+	static const std::regex regex_redirect("(.*)\\s>{1,2}\\s([\\w\\._]+)");
+	static std::cmatch match, m_redirect;
+
+	size_t written, read, run = 1;
+
+	const char* hello = "****KIV-OS****\n";
+	kiv_os_rtl::Write_File(stdinn, hello, strlen(hello), written);
+
+	char *input = (char *)calloc(MAX_SIZE_BUFFER_IN, sizeof(char));
+	while (run) {
+		kiv_os_rtl::Read_File(stdio, input, MAX_SIZE_BUFFER_IN, read);
+
+		if (std::regex_search(input, match, regex_cmd) && (match.size() == REGEX_DEF_GROUP)) {
+			
+			char *params = copy_string(match[2].str());
+			if (!match[2].str().empty() && std::regex_search(params, m_redirect, regex_redirect)) {
+				const char* msg = "\tRedirect found\n";
+				kiv_os_rtl::Write_File(stdinn, msg, strlen(msg), error_write);
+				char *new_params = copy_string(m_redirect[1].str());
+				free(params);
+				params = new_params;
+			}
+
+			char *cmd_name = copy_string(match[1].str());
+			if (call_cmd_function(cmd_name, params) == ERROR_RESULT) {
+				kiv_os_rtl::Write_File(stdinn, error_dialog, strlen(error_dialog), error_write);
+			}
+
+			free(cmd_name);
+			free(params);
+		}
+		else {
+			kiv_os_rtl::Write_File(stdinn, error_dialog, strlen(error_dialog), error_write);
+		}
+
+		input = (char *)calloc(MAX_SIZE_BUFFER_IN, sizeof(char));
+	}
+			
+	kiv_os_rtl::Close_File(stdinn);
+	free(input);
+	
 	return 0;
 }
 
-int8_t call_cmd_function(const char* name, const kiv_os::TRegisters &regs)
+size_t __stdcall echo(const kiv_os::TRegisters &regs)
+{
+	const char* params = reinterpret_cast<char*>(regs.rdx.r);
+	size_t writen;
+
+	if (strlen(params) == 0) {
+		kiv_os_rtl::Write_File(stdinn, error_dialog, strlen(error_dialog), error_write);
+		return ERROR_RESULT;
+	}
+
+	kiv_os_rtl::Write_File(stdinn, params, strlen(params), writen);
+
+	return CORRECT_RESULT;
+}
+
+int8_t call_cmd_function(char* cmd_name, const char* params)
 {
 	uint8_t i, size = sizeof(Cmd_Function) / sizeof(Cmd_Function[0]);
+	kiv_os::TRegisters regs;
+	regs.rdx.r = reinterpret_cast<decltype(regs.rdx.r)>(params);
+	string_to_lower(cmd_name);
 
 	for (i = 0; i < size; i++) {
-		if (Cmd_Function[i].cmd_fun && (strcmp(name, Cmd_Function[i].name) == 0)) {
+		if (Cmd_Function[i].cmd_fun && (strcmp(cmd_name, Cmd_Function[i].name) == 0)) {
 			Cmd_Function[i].cmd_fun(regs);
 			return 0;
 		}
 	}
 
 	return -1;
+}
+
+void string_to_lower(char* str)
+{
+	size_t len = strlen(str);
+	for (size_t i = 0; i < len; i++) {
+		str[i] = tolower(str[i]);
+	}
+}
+
+char *copy_string(const std::string &str)
+{
+	size_t len = str.size();
+	char *new_str = (char*)malloc(sizeof(char) * (len + 1u));
+	strcpy_s(new_str, len + 1u, str.c_str());
+	new_str[len] = '\0';
+	
+	return new_str;
 }
--- src/user/shell.h
+++ src/user/shell.h
@@ -1,6 +1,21 @@
 #pragma once
 
 #include "..\api\api.h"
+#include "rtl.h"
+#include <string>
+
+#define ERROR_RESULT		-1
+#define CORRECT_RESULT		1
+
+#define MAX_SIZE_BUFFER_IN	1024
+#define REGEX_DEF_GROUP		3
+
+static const char* error_dialog = "The syntax of the command is incorrect.\n";
+static const char* cmd_unknown_dialog = "Unknown command...\n";
+
+static const kiv_os::THandle stdinn = kiv_os_rtl::Create_File("CONOUT$", /*FILE_SHARE_WRITE*/2);
+static const kiv_os::THandle stdio = kiv_os_rtl::Create_File("CONIN$", /*FILE_SHARE_READ*/1);
+static size_t error_write;
 
 extern "C" size_t __stdcall shell(const kiv_os::TRegisters &regs);
 
@@ -9,13 +24,14 @@ extern "C" size_t __stdcall shell(const kiv_os::TRegisters &regs);
 extern "C" size_t __stdcall type(const kiv_os::TRegisters &regs) { return 0; };
 extern "C" size_t __stdcall md(const kiv_os::TRegisters &regs) { return 0; }
 extern "C" size_t __stdcall rd(const kiv_os::TRegisters &regs) { return 0; }
-extern "C" size_t __stdcall echo(const kiv_os::TRegisters &regs) { return 0; }
+extern "C" size_t __stdcall echo(const kiv_os::TRegisters &regs);
 extern "C" size_t __stdcall wc(const kiv_os::TRegisters &regs) { return 0; }
 extern "C" size_t __stdcall sort(const kiv_os::TRegisters &regs) { return 0; }
 extern "C" size_t __stdcall rgen(const kiv_os::TRegisters &regs) { return 0; }
 extern "C" size_t __stdcall freq(const kiv_os::TRegisters &regs) { return 0; }
 extern "C" size_t __stdcall ps(const kiv_os::TRegisters &regs) { return 0; }
 extern "C" size_t __stdcall shutdown(const kiv_os::TRegisters &regs) { return 0; }
+
 
 
 /* Tabulka muze byt max. velikosti uint8_t => 256 - pro nase ucely postacujici */
@@ -27,6 +43,7 @@ const static struct {
 	{"md", md},
 	{"rd", rd},
 	{"echo", echo},
+	{"echo.", echo},	//TODO: promyslet, jestli neudelat tuple?? zalezi, jestli existuje vice variant zapisu i u ostatnich
 	{"wc", wc},
 	{"sort", sort},
 	{"rgen", rgen},
@@ -35,4 +52,12 @@ const static struct {
 	{"shutdown", shutdown}
 };
 
-int8_t call_cmd_function(const char* name, const kiv_os::TRegisters &regs);
+int8_t call_cmd_function(char* cmd_name, const char* params);
+
+/* Convsert c-string to lower case */
+void string_to_lower(char* str);
+
+/* Copy std::string to char* */
+char *copy_string(const std::string &str);
+
+
