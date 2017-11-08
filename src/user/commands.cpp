a85f241ab3174e5c9ce2006c7d47337867498cc4
kanakl <ludek.kanak@seznam.cz>
2017-11-09 00:12:16 +0100

    Processes #12
    Change std names

--- src/user/commands.cpp
+++ src/user/commands.cpp
@@ -19,10 +19,10 @@ size_t __stdcall rd(const kiv_os::TRegisters &regs) {
 size_t __stdcall echo(const kiv_os::TRegisters &regs) {
 	kiv_os::TProcess_Startup_Info *tsi = reinterpret_cast<kiv_os::TProcess_Startup_Info*> (regs.rdi.r);
 	const char* params = tsi->arg;
-	kiv_os::THandle stdout = tsi->stdout_t;
+	kiv_os::THandle stdout_t = tsi->stdout_t;
 	size_t writen;	
 
-	kiv_os_rtl::Write_File(stdout, params, strlen(params), writen);
+	kiv_os_rtl::Write_File(stdout_t, params, strlen(params), writen);
 	
 	return writen;
 }
