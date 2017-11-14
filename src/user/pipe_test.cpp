0d48ef158de257a97cea0e2fd75e34ec3bb2554d
Patrik Patera <pat.patera@gmail.com>
2017-11-14 03:58:02 +0100

    Test pipe and redirect #14
    +Added basic test with delay for pipe.

--- src/user/pipe_test.cpp
+++ src/user/pipe_test.cpp
@@ -3,10 +3,15 @@
 #include <iostream>
 #include <string.h>
 #include <memory>
+#include <string>
+#include <iostream>
 
+std::mutex m;
 
 void pipe_test() 
 {
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	print_smt("****PIPE_TEST_WITH_DELAY_READER****");
 	kiv_os::THandle pipe_handles[2];
 	assert(pipe_handles);
 	bool result = kiv_os_rtl::Create_Pipe(pipe_handles);
@@ -19,56 +24,64 @@ void pipe_test()
 	r.join();
 	w.join();
 
-	std::cout << "END PIPE TEST" << std::endl;
+	print_smt("***********************************");
 }
 
 
 void pipe1(const kiv_os::THandle& writer)
 {
-	std::cout << "CREATE WRITER" << std::endl;
+	print_smt("CREATED WRITER");
 
-	const char *write = "Tak tohle chci zapsat", *write2 = "dsjkfsdlafjslkfd";
-	std::cout << "\tWRITER - wait 3s" << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds(3));
+	const char *write = "Tohle je testovaci string, jeslti se zapis a cteni v cajku?!";
+	//std::this_thread::sleep_for(std::chrono::seconds(3));
 	
 	size_t written;
 	bool res = kiv_os_rtl::Write_File(writer, write, strlen(write), written);
-	std::cout << "\tWRITTEN - " << write << "[" << written << "]" << std::endl;
+	std::string s1 = "\tWRITTEN -> " + std::string(write) /*+ "[" + std::to_string(written) + "]"*/;
+	print_smt(s1);
 
 	assert(res);
 
-	std::cout << "\tWRITER - wait 3s" << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds(3));
-	res = kiv_os_rtl::Write_File(writer, write2, strlen(write2), written);
-	std::cout << "\tWRITTEN - " << write2 << "[" << written << "]" << std::endl;
-
 	kiv_os_rtl::Close_File(writer);
-	std::cout << "WRITER CLOSE HANDLER" << std::endl;
+	print_smt("WRITER CLOSED HANDLER");
 }
 
 void pipe2(const kiv_os::THandle& reader)
 {
-	std::cout << "CREATE READER" << std::endl;
-	size_t size_read = 400;
+	print_smt("CREATED READER");
+	size_t size_read = 10;
 	char *read = new char[size_read];
 
 	size_t read_r;
 	int i = 1;
 
 	bool res = false;
+	std::string res_str = "";
 
 	do {
-		res = kiv_os_rtl::Read_File(reader, read, 400, read_r);
+		res = kiv_os_rtl::Read_File(reader, read, size_read, read_r);
 		if (res && read_r > 0) {
-			std::cout << "\tREADING(" << i << "): " << read << std::endl;
+			res_str += std::string(read);
 			i++;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
 		}
 	} while (res);
 
 	if (!res && read_r == 0) {
-		std::cout << "\tREADING(" << i << "): EOF" << std::endl;
+		print_smt("\tREADER - EOF");
 		kiv_os_rtl::Close_File(reader);
+		{
+			std::lock_guard<std::mutex> l(m);
+			std::cout << "\tREAD -> " << res_str << std::endl;
+		}
+		print_smt("READER CLOSED HANDLER");
 	}
 
 	delete[] read;		/* Here coulde be problem in debug because DLL lib has diferent heap than the main program.*/
+}
+
+void print_smt(const std::string& str)
+{
+	std::lock_guard<std::mutex> lock(m);
+	std::cout << str << std::endl;
 }
--- src/user/pipe_test.h
+++ src/user/pipe_test.h
@@ -4,11 +4,13 @@
 
 #include <cassert>
 #include <thread>
+#include <mutex>
 
 
 void pipe_test();
 
 void pipe1(const kiv_os::THandle& writer);
 void pipe2(const kiv_os::THandle& reader);
+void print_smt(const std::string& str);
 
 
