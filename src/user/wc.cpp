ca0bad400015b3edaa3c1d8699bf947c1bfdb134
Patrik Patera <pat.patera@gmail.com>
2017-11-15 02:38:18 +0100

    Command wc #48
    +Fixed for read from console but need to uncomment after open a file.

--- src/user/wc.cpp
+++ src/user/wc.cpp
@@ -1,21 +1,51 @@
 #include "wc.h"
 #include "rtl.h"
-#include "string.h"
 
 #include <regex>
-#include <iostream>
 
 
 size_t __stdcall wc(const kiv_os::TRegisters &regs) {
-	//char* params = reinterpret_cast<char*> (regs.rdi.r);
-	char *params = "Ahoj\n jak se mas?\nJa\ndobre, haha";			//TODO get params from regs
-	std::string str(params);
+	kiv_os::TProcess_Startup_Info *tsi = reinterpret_cast<kiv_os::TProcess_Startup_Info*> (regs.rdi.r);
+	char *str = tsi->arg;		
 
+	kiv_os::THandle handle = kiv_os::stdInput;		//TODO DELETE this definition, only declaration;
+	if (strlen(str) == 0) {
+		handle = kiv_os::stdInput;
+	}
+	else {
+		//handle = kiv_os_rtl::Create_File(str, kiv_os::fmOpen_Always);		//TODO open file on disk
+	}
+
+	size_t read, lines = 0, words = 0, characters = 0;
+
+	char *input = (char *)malloc(1024 * sizeof(char));
+	bool res = true;
+	do {
+		res = kiv_os_rtl::Read_File(handle, input, 1024, read);
+		std::string str_input(input, read);
+
+		calculate(str_input, lines, words);
+
+		characters += read;
+	} while (res && read > 0);
+
+	std::string out = "\t" + std::to_string(lines) + " \t" + std::to_string(words) + " \t" + std::to_string(characters) + "\n";	//TODO FILE NAME
+	const char *output = kiv_os_str::copy_string(out);
+
+	size_t written;
+	kiv_os_rtl::Write_File(kiv_os::stdOutput, output, out.size(), written);
+
+	free(str);
+	str = NULL;		//TODO clear this pointer?
+
+	return 0;
+}
+
+void calculate(std::string &str, size_t &lines, size_t &words)
+{
 	std::smatch match;
-	std::regex reg("([^\\s]*)(\\s*)");
+	static std::regex reg("([^\\s]*)(\\s*)");
 
-	size_t lines = 0, words = 0, characters = 0;
-	bool gap = false;
 	while (!str.empty() && std::regex_search(str, match, reg)) {
 		if (!match[2].str().empty()) {
 			std::string tmp = match[2].str();
@@ -23,18 +53,6 @@ size_t __stdcall wc(const kiv_os::TRegisters &regs) {
 		}
 
 		words++;
-		characters += match[1].str().length() + match[2].str().length();
 		str = match.suffix();
 	}
-
-	str = std::to_string(lines) + " " + std::to_string(words) + " " + std::to_string(characters) + "\n";	//TODO FILE NAME
-	params = kiv_os_str::copy_string(str);
-
-	size_t written;
-	kiv_os_rtl::Write_File(2, params, str.size(), written);		//TODO SET OUTPUT FROM REGS
-
-	free(params);
-	params = NULL;
-
-	return 0;
 }
--- src/user/wc.h
+++ src/user/wc.h
@@ -1,3 +1,6 @@
 #include "../api/api.h"
+#include "string.h"
 
 extern "C" size_t __stdcall wc(const kiv_os::TRegisters &regs);
+
+inline void calculate(std::string &str, size_t &lines, size_t &words);
