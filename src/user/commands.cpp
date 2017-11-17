4106dc6b433ea584cbf29ecbda691e59c72b7a2e
kanakl <ludek.kanak@seznam.cz>
2017-11-17 21:32:59 +0100

    Echo closing

--- src/user/commands.cpp
+++ src/user/commands.cpp
@@ -14,11 +14,11 @@ size_t __stdcall rd(const kiv_os::TRegisters &regs) {
 size_t __stdcall echo(const kiv_os::TRegisters &regs) {
 	kiv_os::TProcess_Startup_Info *tsi = reinterpret_cast<kiv_os::TProcess_Startup_Info*> (regs.rdi.r);
 	const char* params = tsi->arg;
-	kiv_os::THandle stdout_t = tsi->stdout_t;
+
 	size_t writen;	
 
-	kiv_os_rtl::Write_File(stdout_t, params, strlen(params), writen);
-	
+	kiv_os_rtl::Write_File(kiv_os::stdOutput, params, strlen(params), writen);
+	kiv_os_rtl::Close_File(kiv_os::stdOutput);
 	return writen;
 }
 
