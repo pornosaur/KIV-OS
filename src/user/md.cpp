5d34a8fa515eee27d81a74b0a3d00ca6a67ad877
Tomáš Šimandl <tomas.simandl@gmail.com>
2017-11-18 12:47:54 +0100

    command md #73
    + create directories recursively

--- src/user/md.cpp
+++ src/user/md.cpp
@@ -1,30 +1,23 @@
 #include "md.h"
 
-size_t __stdcall md(const kiv_os::TRegisters &regs) {
+size_t __stdcall md(const kiv_os::TRegisters &regs)
+{
 	kiv_os::TProcess_Startup_Info *tsi = reinterpret_cast<kiv_os::TProcess_Startup_Info*> (regs.rdi.r);
 	kiv_os::THandle stdout_t = tsi->stdout_t;
 	char* params = tsi->arg;
 
 	std::smatch match;
 	std::string str(params);
-	kiv_os::THandle handle;
 
-	while (!str.empty() && std::regex_search(str, match, reg_md)) {
+	while (!str.empty() && std::regex_search(str, match, reg_md_multi)) {
 		std::string tmp = match[0].str();
 		str = match.suffix();
 
 		if (!tmp.empty()) {
-			tmp.erase(tmp.find_last_not_of(" \n\r\t\"'") + 1);
-			tmp.erase(0, tmp.find_first_not_of(" \n\r\t\"'"));
+			tmp.erase(tmp.find_last_not_of(erase_chars) + 1);
+			tmp.erase(0, tmp.find_first_not_of(erase_chars));
 
-
-			handle = kiv_os_rtl::Create_File(tmp.c_str(), kiv_os::fmOpen_Always, kiv_os::faDirectory);
-			if (!handle) {
-				return 0; // TODO error
-			}
-
-			kiv_os_rtl::Close_File(handle);
-
+			create_directories(tmp);
 		}
 	}
 
@@ -32,4 +25,44 @@ size_t __stdcall md(const kiv_os::TRegisters &regs) {
 	params = NULL;		//TODO clear this pointer?
 
 	return 0; // TODO what return
+}
+
+void create_directories(std::string &path)
+{
+	bool created = false;
+	
+	kiv_os::THandle handle;
+	std::smatch match;
+	std::string tmp_path;
+
+	while (!path.empty() && std::regex_search(path, match, reg_md_recur)) {
+		std::string tmp(match[0].str());
+		path = match.suffix();
+
+		if (!tmp.empty()) {
+			tmp_path.append(tmp);
+			
+			// open dir if exists
+			handle = kiv_os_rtl::Create_File(tmp_path.c_str(), kiv_os::fmOpen_Always, kiv_os::faDirectory);
+			if (handle) {
+				kiv_os_rtl::Close_File(handle);
+				tmp_path.append("/");
+				continue;
+			}
+
+			// create dir
+			handle = kiv_os_rtl::Create_File(tmp_path.c_str(), 0, kiv_os::faDirectory);
+			if (!handle) {
+				return; // TODO error
+			}
+
+			kiv_os_rtl::Close_File(handle);
+			tmp_path.append("/");
+			created = true;
+		}
+	}
+
+	if (!created) {
+		return; // TODO return error alredy exists
+	}
 }
--- src/user/md.h
+++ src/user/md.h
@@ -4,6 +4,11 @@
 
 #include <regex>
 
-static std::regex reg_md("\"([\\S\\s][^\"]+)\"|\'([\\S\\s][^\']+)\'|(\\S+)");
+#define erase_chars " \n\r\t\"'/"
+
+static std::regex reg_md_multi("\"([\\S\\s][^\"]+)\"|\'([\\S\\s][^\']+)\'|(\\S+)");
+static std::regex reg_md_recur("[^\\/]+");
 
 extern "C" size_t __stdcall md(const kiv_os::TRegisters &regs);
+
+void create_directories(std::string &path);
