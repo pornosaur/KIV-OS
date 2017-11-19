c84407111834cbb0f81c8032e5252e82999d2af8
Tomáš Šimandl <tomas.simandl@gmail.com>
2017-11-19 14:48:48 +0100

    command rd #76
    + error messages
    + ask for delete in recursive mode

--- src/user/rd.cpp
+++ src/user/rd.cpp
@@ -8,6 +8,9 @@ size_t __stdcall rd(const kiv_os::TRegisters &regs)
 	std::smatch match;
 	std::string str(params);
 
+	free(params);
+	params = NULL;		//TODO clear this pointer?
+
 	bool quiet = false;
 	bool recursively = false;
 
@@ -24,56 +27,68 @@ size_t __stdcall rd(const kiv_os::TRegisters &regs)
 			// Check if folder exists and if it is a directory
 			kiv_os::THandle handle = kiv_os_rtl::Create_File(tmp.c_str(), kiv_os::fmOpen_Always, kiv_os::faDirectory);
 			if (!handle) {
-				return 0; // TODO error Nazev adresare je neplatny
+				rd_print_error();
+				return 0;
 			}
 
-			kiv_os_rtl::Close_File(handle); // TODO check return code of Close_File?
+			bool res = kiv_os_rtl::Close_File(handle);
+			if (!res) {
+				rd_print_error();
+				return 0;
+			}
 
 			if (recursively) {
-				if (!quiet) {
-					// TODO ask for deletion
+				if (!quiet && ask_for_deletion(tmp))
+				{
+					res = remove_recursively(tmp, regs);
+					if (!res) {
+						rd_print_error();
+						return 0;
+					}
 				}
-				remove_recursively(tmp, regs);
 			}
 			else {
-				bool res = kiv_os_rtl::Remove_File(tmp.c_str());
+				res = kiv_os_rtl::Remove_File(tmp.c_str());
 				if (!res) {
-					return 0; // TODO error
+					rd_print_error();
+					return 0;
 				}
 			}
 		}
 	}
 
-	free(params);
-	params = NULL;		//TODO clear this pointer?
-
 	return 0; // TODO what return
 }
 
-void remove_recursively(std::string &path, const kiv_os::TRegisters &regs) {
+bool remove_recursively(std::string &path, const kiv_os::TRegisters &regs) {
 	
 	bool res = kiv_os_rtl::Remove_File(path.c_str());
 	if (res) {
-		return; // succesly removed
+		return true; // succesly removed
 	}
 
 	if (kiv_os_rtl::Get_Last_Error() != kiv_os::erDir_Not_Empty) {
-		return; // TODO error
+		return false;
 	}
 
-	remove_subfiles(path, regs);
+	res = remove_subfiles(path, regs);
+	if (!res) {
+		return false;
+	}
 
 	res = kiv_os_rtl::Remove_File(path.c_str());
 	if (!res) {
-		return; // TODO error
+		return false;
 	}
+
+	return true;
 }
 
-void remove_subfiles(std::string path, const kiv_os::TRegisters &regs)
+bool remove_subfiles(std::string path, const kiv_os::TRegisters &regs)
 {
 	kiv_os::THandle handle = kiv_os_rtl::Create_File(path.c_str(), kiv_os::fmOpen_Always, kiv_os::faDirectory);
 	if (!handle) {
-		return; // TODO error
+		return false;
 	}
 
 	size_t input_size = sizeof(kiv_os::TDir_Entry);
@@ -90,16 +105,20 @@ void remove_subfiles(std::string path, const kiv_os::TRegisters &regs)
 		entry = reinterpret_cast<kiv_os::TDir_Entry *>(input);
 		if (entry) {
 			std::string tmp(path);
-			remove_recursively(tmp.append(entry->file_name), regs); // TODO handle error
+			res = remove_recursively(tmp.append(entry->file_name), regs);
 		}
 
 	} while (res && read > 0);
 
 	free(input);
+	if (!kiv_os_rtl::Close_File(handle)) {
+		return false;
+	}
 
 	if (!res) {
-		return; // TODO error
+		return false;
 	}
+	return true;
 }
 
 void check_params(std::string &parameters, bool &recursively, bool &quiet)
@@ -126,5 +145,95 @@ void check_params(std::string &parameters, bool &recursively, bool &quiet)
 		else {
 			break;
 		}
+	}
+}
+
+bool ask_for_deletion(std::string &path) {
+
+	char *input = (char *)malloc(buffer_size * sizeof(char));
+	size_t writen = 0, read = 0;
+	bool res = true;
+
+	while (res) {
+		res = kiv_os_rtl::Write_File(kiv_os::stdOutput, path.c_str(), path.size(), writen);
+		if (!res) {
+			rd_print_error();
+			free(input);
+			return false;
+		}
+		res = kiv_os_rtl::Write_File(kiv_os::stdOutput, ", Are you sure (y/n)? ", 22, writen);
+		if (!res) {
+			rd_print_error();
+			free(input);
+			return false;
+		}
+
+		res = kiv_os_rtl::Read_File(kiv_os::stdInput, input, buffer_size, read);
+		if (!res) {
+			rd_print_error();
+			free(input);
+			return false;
+		}
+
+		if (input[0] == 'y' || input[0] == 'Y') {
+			free(input);
+			return true;
+		}
+		if (input[0] == 'n' || input[0] == 'N') {
+			free(input);
+			return false;
+		}
+	}
+
+	free(input);
+	return false;
+}
+
+void rd_print_error()
+{
+	switch (kiv_os_rtl::Get_Last_Error()) {
+	case kiv_os::erInvalid_Handle:
+		rd_print_msg("Internal error. (Invalid Handle)");
+		break;
+
+	case kiv_os::erInvalid_Argument:
+		rd_print_msg("Invalid input arugments.");
+		break;
+
+	case kiv_os::erFile_Not_Found:
+		rd_print_msg("System can not find path.");
+		break;
+
+	case kiv_os::erDir_Not_Empty:
+		rd_print_msg("Directory is not empty.");
+		break;
+
+	case kiv_os::erNo_Left_Space:
+		rd_print_msg("Out of disk space.");
+		break;
+
+	case kiv_os::erPermission_Denied:
+		rd_print_msg("Operation is not permitted.");
+		break;
+
+	case kiv_os::erOut_Of_Memory:
+		rd_print_msg("Out of memory.");
+		break;
+
+	case kiv_os::erIO:
+		rd_print_msg("Disk error.");
+		break;
+	}
+}
+
+void rd_print_msg(std::string msg)
+{
+	size_t writen = 0;
+
+	msg.append("\n\n");
+
+	bool res = kiv_os_rtl::Write_File(kiv_os::stdOutput, msg.c_str(), msg.size(), writen);
+	if (!res) {
+		return;
 	}
 }
--- src/user/rd.h
+++ src/user/rd.h
@@ -6,12 +6,16 @@
 
 #define erase_chars " \n\r\t\"'\\"
 #define delimeter "\\"
+#define buffer_size 1024
 
 static std::regex reg_rd("\"([\\S\\s][^\"]+)\"|\'([\\S\\s][^\']+)\'|(\\S+)");
 static std::regex reg_rd_params("(/s|/S)|(/q|/Q)|\"([\\S\\s][^\"]+)\"|\'([\\S\\s][^\']+)\'|(\\S+)");
 
 extern "C" size_t __stdcall rd(const kiv_os::TRegisters &regs);
 
-void remove_recursively(std::string &path, const kiv_os::TRegisters &regs);
-void remove_subfiles(std::string path, const kiv_os::TRegisters &regs);
+bool remove_recursively(std::string &path, const kiv_os::TRegisters &regs);
+bool remove_subfiles(std::string path, const kiv_os::TRegisters &regs);
 void check_params(std::string &parameters, bool &recursively, bool &quiet);
+bool ask_for_deletion(std::string &path);
+void rd_print_error();
+void rd_print_msg(std::string msg);
