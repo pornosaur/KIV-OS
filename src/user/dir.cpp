1317c82fef99335784a417db2a53e6f32e89cc4d
Tomáš Šimandl <tomas.simandl@gmail.com>
2017-11-20 15:53:52 +0100

    Command dir #86

---
+++ src/user/dir.cpp
@@ -0,0 +1,164 @@
+#include "dir.h"
+
+#include <vector>
+
+size_t __stdcall dir(const kiv_os::TRegisters &regs)
+{
+	kiv_os::TProcess_Startup_Info *tsi = reinterpret_cast<kiv_os::TProcess_Startup_Info*> (regs.rdi.r);
+	char* params = tsi->arg;
+
+	std::smatch match;
+	std::string str(params);
+
+	bool recursively = false;
+
+	check_params(str, recursively);
+	str.erase(0, str.find_first_not_of(erase_chars));
+
+	if (str == "") {
+		if (!get_current_dir(str)) {
+			return 0;
+		}
+	}
+
+	while (!str.empty() && std::regex_search(str, match, reg_dir)) {
+		std::string tmp = match[0].str();
+		str = match.suffix();
+
+		if (!tmp.empty()) {
+			tmp.erase(tmp.find_last_not_of(erase_chars) + 1);
+			tmp.erase(0, tmp.find_first_not_of(erase_chars));
+
+			if (!print_directory(tmp, regs, recursively)) {
+				kiv_os_rtl::print_error();
+				return 0;
+			}
+		}
+	}
+	size_t writen;
+	if (!kiv_os_rtl::Write_File(kiv_os::stdOutput, "\n", 1, writen)) {
+		kiv_os_rtl::print_error();
+		return 0;
+	}
+
+	return 0; // TODO what return
+}
+
+bool print_directory(std::string path, const kiv_os::TRegisters &regs, bool &recursively)
+{
+	kiv_os::THandle handle = kiv_os_rtl::Create_File(path.c_str(), kiv_os::fmOpen_Always, kiv_os::faDirectory);
+	if (!handle) {
+		return false;
+	}
+
+	size_t input_size = sizeof(kiv_os::TDir_Entry);
+	char *input = (char *)malloc(input_size);
+	if (!input) {
+		kiv_os_rtl::Close_File(handle);
+		kiv_os_rtl::print_error("Out of memory.");
+		return false;
+	}
+	std::vector<kiv_os::TDir_Entry> entries;
+	kiv_os::TDir_Entry *entry;
+	size_t read = 0;
+	bool res;
+
+	print_name(path);
+	path.append(delimeter);
+
+	do {
+		res = kiv_os_rtl::Read_File(handle, input, input_size, read);
+		if (!res || read != input_size) break;
+
+		entry = reinterpret_cast<kiv_os::TDir_Entry *>(input);
+		if (entry) {
+			res = print_entry(entry);
+				
+			if (res && recursively && (entry->file_attributes & kiv_os::faDirectory) == kiv_os::faDirectory) {
+				entries.push_back(*entry);
+			}
+		}
+
+	} while (res);
+
+	free(input);
+	if (!kiv_os_rtl::Close_File(handle)) {
+		return false;
+	}
+
+	if (!res) {
+		return false;
+	}
+
+	for (kiv_os::TDir_Entry en : entries) {
+		std::string tmp(path);
+		if (!print_directory(tmp.append(en.file_name), regs, recursively)) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool print_entry(kiv_os::TDir_Entry *entry)
+{
+	size_t writen;
+
+	std::string name(entry->file_name);
+	if ((entry->file_attributes & kiv_os::faDirectory) == kiv_os::faDirectory) {
+		name.append("\t\t<DIR>");
+	}
+	name.append("\n");
+
+	return kiv_os_rtl::Write_File(kiv_os::stdOutput, name.c_str(), name.length(), writen);
+}
+
+bool print_name(std::string name)
+{
+	size_t writen;
+	return kiv_os_rtl::Write_File(kiv_os::stdOutput, ("\n" + name + "\n\n").c_str(), name.length() + 3, writen);
+}
+
+bool get_current_dir(std::string &path)
+{
+	size_t writen = 0;
+	char *buffer = (char*)malloc(sizeof(char) * buffer_size);
+	if (!buffer) {
+		kiv_os_rtl::print_error("Out of memory.");
+		return false;
+	}
+
+	if (!kiv_os_rtl::Get_Current_Direcotry(buffer, buffer_size, writen))
+	{
+		free(buffer);
+		kiv_os_rtl::print_error();
+		return false;;
+	}
+
+	path = std::string(buffer);
+	free(buffer);
+	return true;
+}
+
+void check_params(std::string &parameters, bool &recursively)
+{
+	std::smatch match;
+
+	bool was_match = false;
+
+	while (!parameters.empty() && std::regex_search(parameters, match, reg_dir)) {
+		
+		if (!match[1].str().empty()) {
+			recursively = true;
+			was_match = true;
+		}
+
+		if (was_match) {
+			parameters = match.suffix();
+			was_match = false;
+		}
+		else {
+			break;
+		}
+	}
+}
---
+++ src/user/dir.h
@@ -0,0 +1,19 @@
+#include "../api/api.h"
+#include "string.h"
+#include "rtl.h"
+
+#include <regex>
+
+#define erase_chars " \n\r\t\"'\\"
+#define delimeter "\\"
+#define buffer_size 1024
+
+static std::regex reg_dir("(/s|/S)|\"([\\S\\s][^\"]+)\"|\'([\\S\\s][^\']+)\'|(\\S+)");
+
+extern "C" size_t __stdcall dir(const kiv_os::TRegisters &regs);
+
+bool print_directory(std::string path, const kiv_os::TRegisters &regs, bool &recursively);
+bool print_entry(kiv_os::TDir_Entry *entry);
+bool print_name(std::string name);
+bool get_current_dir(std::string &path);
+void check_params(std::string &parameters, bool &recursively);
