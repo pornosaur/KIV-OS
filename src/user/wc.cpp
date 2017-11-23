26a929984003aa162c8096b9e51cbac386ab50c5
Patrik Patera <pat.patera@gmail.com>
2017-11-23 02:44:53 +0100

    Cmd WC for multiple files #83
    +Fixed for multiple files

--- src/user/wc.cpp
+++ src/user/wc.cpp
@@ -1,46 +1,93 @@
 #include "wc.h"
 #include "rtl.h"
 
-#include <regex>
 #include <vld.h>
 
+static const std::regex reg_wc("\"([\\S\\s][^\"]+)\"|\'([\\S\\s][^\']+)\'|(\\S+)");
 
 size_t __stdcall wc(const kiv_os::TRegisters &regs) {
 	kiv_os::TProcess_Startup_Info *tsi = reinterpret_cast<kiv_os::TProcess_Startup_Info*> (regs.rdi.r);
 	
-	char *str = tsi->arg;		
+	std::smatch match;
+	std::string params(tsi->arg);
 
-	/*if (strlen(str) == 0) {
+	params.erase(0, params.find_first_not_of(ERASE_CHARS)); // remove redunadat chars from start of string
+
+	kiv_os::THandle handle;
+	bool stdin = false, console = false;
+
+	if (params.empty()) {
+		console = stdin = true;
 		handle = kiv_os::stdInput;
 	}
-	else {
-		handle = kiv_os_rtl::Create_File(str, kiv_os::fmOpen_Always, 0);		//TODO open file on disk
-	}*/
 
+	char *input = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	size_t total_lines = 0, total_words = 0, total_characters = 0;
 
-	size_t read, lines = 0, words = 0, characters = 0;
+	while (stdin || (!params.empty() && std::regex_search(params, match, reg_wc))) {
+		size_t read, lines = 0, words = 0, characters = 0;
 
-	char *input = (char *)malloc(1024 * sizeof(char));
+		std::string tmp = match[0].str();
+		if (!stdin) {
+			params = match.suffix();
 
-	bool res = false, before_word = false;
-	do {
-		res = kiv_os_rtl::Read_File(kiv_os::stdInput, input, 1024, read);
-		std::string str_input(input, read);
+			if (!tmp.empty()) {
+				tmp.erase(tmp.find_last_not_of(ERASE_CHARS) + 1);
+				tmp.erase(0, tmp.find_first_not_of(ERASE_CHARS));
 
-		calculate(str_input, lines, words, before_word);
+				handle = kiv_os_rtl::Create_File(tmp.c_str(), kiv_os::fmOpen_Always, 0);
 
-		characters += read;
-	} while (res && read > 0);
+				if (!handle) {
+					kiv_os_rtl::print_error();
+					return 0;
+				}
+			}
+		}
 
-	if (before_word) {
-		words++;
+		bool res = false, before_word = false;
+		do {
+			res = kiv_os_rtl::Read_File(handle, input, BUFFER_SIZE, read);
+			std::string str_input(input, read);
+
+			calculate(str_input, lines, words, before_word);
+
+			characters += read;
+
+			if (!stdin) {
+				if (!kiv_os_rtl::Close_File(handle)) {
+					kiv_os_rtl::print_error();
+					return 0;
+				}
+
+				read = 0;
+			}
+
+		} while (res && read > 0);
+
+		if (before_word) {
+			words++;
+		}
+
+		total_words += words;
+		total_lines += lines;
+		total_characters += characters;
+
+		std::string out = "\t" + std::to_string(lines) + " \t" + std::to_string(words)
+			+ " \t" + std::to_string(characters) + " " + tmp + "\n";
+
+		size_t written;
+		kiv_os_rtl::Write_File(kiv_os::stdOutput, out.c_str(), out.size(), written);
+
+		stdin = false;
 	}
 
-	std::string out = "\t" + std::to_string(lines) + " \t" + std::to_string(words)
-		+ " \t" + std::to_string(characters) + " " + str +"\n";
+	if (!console) {
+		std::string out = "\t" + std::to_string(total_lines) + " \t" + std::to_string(total_words)
+			+ " \t" + std::to_string(total_characters) + " total\n";
 
-	size_t written;
-	kiv_os_rtl::Write_File(kiv_os::stdOutput, out.c_str(), out.size(), written);
+		size_t written;
+		kiv_os_rtl::Write_File(kiv_os::stdOutput, out.c_str(), out.size(), written);
+	}
 
 	free(input);
 	input = nullptr;
--- src/user/wc.h
+++ src/user/wc.h
@@ -2,8 +2,17 @@
 #undef stdin
 #undef stdout
 #undef stderr
+
 #include "../api/api.h"
 #include "string.h"
+
+#include <regex>
+
+#define ERASE_CHARS " \n\r\t\"'\\"
+/* path delimeter */
+#define DELIMETER "\\"
+/* size of read buffer */
+#define BUFFER_SIZE 1024
 
 extern "C" size_t __stdcall wc(const kiv_os::TRegisters &regs);
 
