bda21b3a345899a7b06a7c5f5ed4c3df7d2e2867
Patrik Patera <pat.patera@gmail.com>
2017-11-26 23:19:54 +0100

    API doc #94
    +Added comment to PipeHandler

--- src/kernel/PipeHandler.h
+++ src/kernel/PipeHandler.h
@@ -4,13 +4,47 @@
 class PipeHandler : public Handler {
 
 private: 
+	/** Appropriate pipe */
 	Pipe* pipe;
 
 public:
+	/**
+	 * Create pipe hadnler with exists pipe.
+	 *
+	 * @param pipe appropriate pipe
+	 * @param flags identifies pipe handler for read or write
+	 */
 	PipeHandler(Pipe* pipe, uint8_t flags) : Handler(flags), pipe(pipe) {};
 	~PipeHandler();
+
+	/**
+	 * Read from pipe buffer to output buffer.
+	 *
+	 * @param buffer output buffer
+	 * @param length size of output buffer
+	 * @param read read byte
+	 * @return error code
+	 */
 	virtual uint16_t read(char* buffer, size_t length, size_t& read);
+
+	/**
+	 * Write to the pipe buffer from input buffer.
+	 *
+	 * @param buffer input buffer
+	 * @param length size of input buffer
+	 * @param written written byte to input buffer
+	 * @return error code
+	 */
 	virtual uint16_t write(char* buffer, size_t length, size_t& written);
+
+	/**
+	* Method for set position in pipe buffer.
+	*
+	* @param offset number of bytes to offset from origin
+	* @param origin position which reference for offset
+	* @param set_size if is set the position will be set as a size
+	* @return error code
+	*/
 	virtual uint16_t fseek(long offset, uint8_t origin, uint8_t set_size);
 
 };
